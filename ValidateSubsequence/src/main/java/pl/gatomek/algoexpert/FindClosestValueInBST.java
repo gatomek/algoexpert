81eb8fa80db59d597f1dfca784ad89288099ee76
Gatomek <tomasz.galinski@gmail.com>
2026-09-17 18:36:31 +0200

    FindClosestValueInBST added

---
+++ ValidateSubsequence/src/main/java/pl/gatomek/algoexpert/FindClosestValueInBST.java
@@ -0,0 +1,42 @@
+package pl.gatomek.algoexpert;
+
+public class FindClosestValueInBST {
+    static int findClosestValueInBst(BST tree, int target) {
+        final int currValue = tree.value;
+        if( currValue == target) {
+            return currValue;
+        }
+
+        if( target < currValue) {
+            if( tree.left != null) {
+                final int candidate = findClosestValueInBst( tree.left, target);
+                int currDelta = Math.abs(target - currValue);
+                int candDelta = Math.abs(target - candidate);
+                return currDelta < candDelta ? currValue : candidate;
+            }
+            else {
+                return currValue;
+            }
+        }
+        else {
+            if( tree.right != null) {
+                final int candidate = findClosestValueInBst( tree.right, target);
+                int currDelta = Math.abs(target - currValue);
+                int candDelta = Math.abs(target - candidate);
+                return currDelta < candDelta ? currValue : candidate;
+            } else {
+                return currValue;
+            }
+        }
+    }
+
+    static class BST {
+        public int value;
+        public BST left;
+        public BST right;
+
+        public BST(int value) {
+            this.value = value;
+        }
+    }
+}
