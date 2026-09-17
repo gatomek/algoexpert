2036e9acb82ae3e2188203025b930369042dea0c
Gatomek <tomasz.galinski@gmail.com>
2026-09-17 23:59:54 +0200

    cr

--- src/main/java/pl/gatomek/algoexpert/NodeDepths.java
+++ src/main/java/pl/gatomek/algoexpert/NodeDepths.java
@@ -1,22 +1,26 @@
 package pl.gatomek.algoexpert;
 
 public class NodeDepths {
+
+    private NodeDepths() {
+    }
+
     static int nodeDepths(BinaryTree root) {
         return preOrderTraverse(root, 0);
     }
 
     static int preOrderTraverse(BinaryTree node, int level) {
-        int currentLevel = level;
+        int currentTotal = level;
 
-        if(node.left != null) {
-            currentLevel += preOrderTraverse(node.left, level + 1);
+        if (node.left != null) {
+            currentTotal += preOrderTraverse(node.left, level + 1);
         }
 
-        if(node.right != null) {
-            currentLevel += preOrderTraverse(node.right, level + 1);
+        if (node.right != null) {
+            currentTotal += preOrderTraverse(node.right, level + 1);
         }
 
-        return currentLevel;
+        return currentTotal;
     }
 
     static class BinaryTree {
