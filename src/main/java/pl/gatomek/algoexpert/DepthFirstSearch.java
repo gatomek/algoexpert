5868a94c2a80bb8a209aee5adaa1b98fd7485b24
Gatomek <37158592+gatomek@users.noreply.github.com>
2026-09-18 11:12:34 +0200

    Depth-First Seach (#14)

---
+++ src/main/java/pl/gatomek/algoexpert/DepthFirstSearch.java
@@ -0,0 +1,33 @@
+package pl.gatomek.algoexpert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// todo: add non-recursive method
+
+public class DepthFirstSearch {
+    static class Node {
+        String name;
+        List<Node> children = new ArrayList<>();
+
+        public Node(String name) {
+            this.name = name;
+        }
+
+        public List<String> depthFirstSearch(List<String> array) {
+            array.add(name);
+
+            for (Node n : children) {
+                n.depthFirstSearch(array);
+            }
+
+            return array;
+        }
+
+        public Node addChild(String name) {
+            Node child = new Node(name);
+            children.add(child);
+            return this;
+        }
+    }
+}
