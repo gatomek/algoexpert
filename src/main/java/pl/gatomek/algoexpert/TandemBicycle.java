21b74ffd79f1929d08196ad5a75c6b567cc37d59
Gatomek <37158592+gatomek@users.noreply.github.com>
2026-09-18 16:11:13 +0200

    Tandem Bicycle added (#17)

---
+++ src/main/java/pl/gatomek/algoexpert/TandemBicycle.java
@@ -0,0 +1,26 @@
+package pl.gatomek.algoexpert;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class TandemBicycle {
+    public int tandemBicycle(
+            int[] redShirtSpeeds, int[] blueShirtSpeeds, boolean fastest
+    ) {
+        Integer[] reds = Arrays.stream(redShirtSpeeds).boxed().sorted().toArray(Integer[]::new);
+        int speedSum = 0;
+
+        Integer[] blues;
+        if (fastest) {
+            blues = Arrays.stream(blueShirtSpeeds).boxed().sorted(Collections.reverseOrder()).toArray(Integer[]::new);
+        } else {
+            blues = Arrays.stream(blueShirtSpeeds).boxed().sorted().toArray(Integer[]::new);
+        }
+
+        for (int i = 0; i < reds.length; i++) {
+            speedSum += Math.max(reds[i], blues[i]);
+        }
+
+        return speedSum;
+    }
+}
