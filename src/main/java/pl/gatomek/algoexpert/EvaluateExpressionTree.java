4e3dbd4fbf0b1dce2c9fdc712892b84607c09fb8
Gatomek <37158592+gatomek@users.noreply.github.com>
2026-09-18 09:51:23 +0200

    Evaluate Expression Tree added (#13)

---
+++ src/main/java/pl/gatomek/algoexpert/EvaluateExpressionTree.java
@@ -0,0 +1,38 @@
+package pl.gatomek.algoexpert;
+
+//todo: algo without recurrency?
+
+public class EvaluateExpressionTree {
+    static class BinaryTree {
+        public int value;
+        public BinaryTree left = null;
+        public BinaryTree right = null;
+
+        public BinaryTree(int value) {
+            this.value = value;
+        }
+    }
+
+    int evaluateExpressionTree(BinaryTree tree) {
+        return postOrderCalc(tree);
+    }
+
+    int postOrderCalc(BinaryTree tree) {
+        boolean isOperator = tree.value < 0;
+        if (isOperator) {
+            int a = postOrderCalc(tree.left);
+            int b = postOrderCalc(tree.right);
+
+            return switch(tree.value) {
+                case -1 -> a+b;
+                case -2 -> a-b;
+                case -3 -> a/b;
+                case -4 -> a*b;
+                default -> throw new IllegalStateException("Unknown Operator");
+            };
+        }
+        else {
+            return tree.value;
+        }
+    }
+}
