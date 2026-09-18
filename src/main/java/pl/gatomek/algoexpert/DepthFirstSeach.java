f156a866e22d0f393948615236d45cfd11c9984e
Gatomek <tomasz.galinski@gmail.com>
2026-09-18 11:04:27 +0200

    Depth-First Seach

---
+++ src/main/java/pl/gatomek/algoexpert/DepthFirstSeach.java
@@ -0,0 +1,31 @@
+package pl.gatomek.algoexpert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DepthFirstSeach {
+    static class Node {
+        String name;
+        List<Node> children = new ArrayList<>();
+
+        public Node(String name) {
+            this.name = name;
+        }
+
+        public List<String> depthFirstSearch(List<String> array) {
+            array.add(name);
+
+            for (Node n : children) {
+                array = n.depthFirstSearch(array);
+            }
+
+            return array;
+        }
+
+        public Node addChild(String name) {
+            Node child = new Node(name);
+            children.add(child);
+            return this;
+        }
+    }
+}
