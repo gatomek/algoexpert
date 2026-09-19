145ef01e8364c9c3eb79d56259a232f9781060b2
Gatomek <tomasz.galinski@gmail.com>
2026-09-19 13:09:29 +0200

    Product Sum added

---
+++ src/main/java/pl/gatomek/algoexpert/ProductSum.java
@@ -0,0 +1,24 @@
+package pl.gatomek.algoexpert;
+
+import java.util.List;
+
+public class ProductSum {
+    public static int calcSum(List<Object> array, int level) {
+        int sum = 0;
+        for (Object o : array) {
+            if (o instanceof List) {
+
+                @SuppressWarnings("unchecked")
+                List<Object> list = (List<Object>) o;
+                sum += calcSum(list, level + 1);
+            } else {
+                sum += (Integer) o;
+            }
+        }
+        return level * sum;
+    }
+
+    public static int productSum(List<Object> array) {
+        return calcSum(array, 1);
+    }
+}
