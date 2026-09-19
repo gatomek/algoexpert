1dc1d2cdfaa2c84a9ca7ec452cc4254b1c0f4bea
Gatomek <37158592+gatomek@users.noreply.github.com>
2026-09-19 09:15:23 +0200

    Remove Duplicates From Linked List added (#19)

---
+++ src/main/java/pl/gatomek/algoexpert/RemoveDuplicatesFromLinkedList.java
@@ -0,0 +1,61 @@
+package pl.gatomek.algoexpert;
+
+// todo: performance comparison of recursive and non-recursive methods
+
+public class RemoveDuplicatesFromLinkedList {
+    private void stepOver(LinkedList currentNode) {
+        LinkedList nextNode = currentNode.next;
+        if (nextNode == null) {
+            return;
+        }
+
+        final int currentValue = currentNode.value;
+        final int nextValue = nextNode.value;
+
+        if (currentValue != nextValue) {
+            stepOver(nextNode);
+        } else {
+            currentNode.next = nextNode.next;
+            nextNode.next = null;
+            stepOver(currentNode);
+        }
+    }
+
+    public LinkedList removeDuplicatesFromLinkedListRecursive(LinkedList linkedList) {
+        stepOver(linkedList);
+        return linkedList;
+    }
+
+    public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+        LinkedList currentNode = linkedList;
+        while (currentNode != null) {
+
+            LinkedList nextNode = currentNode.next;
+            if (nextNode == null) {
+                break;
+            }
+
+            int currentValue = currentNode.value;
+            int nextValue = nextNode.value;
+
+            if (currentValue != nextValue) {
+                currentNode = nextNode;
+            }
+            else {
+                currentNode.next = nextNode.next;
+                nextNode.next = null;
+            }
+        }
+
+        return linkedList;
+    }
+    public static class LinkedList {
+        public int value;
+        public LinkedList next;
+
+        public LinkedList(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+}
