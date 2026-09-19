7dfce0f185563dadd27f17b8c436d7bb19e46c15
Gatomek <tomasz.galinski@gmail.com>
2026-09-19 08:45:25 +0200

    Remove Duplicates From Linked List added

---
+++ src/main/java/pl/gatomek/algoexpert/RemoveDuplicatesFromLinkedList.java
@@ -0,0 +1,36 @@
+package pl.gatomek.algoexpert;
+
+public class RemoveDuplicatesFromLinkedList {
+    public void stepOver(LinkedList currentNode) {
+        LinkedList nextNode = currentNode.next;
+        if (nextNode == null) {
+            return;
+        }
+
+        final int currentValue = currentNode.value;
+        final int nextValue = nextNode.value;
+
+        if (currentValue != nextValue) {
+            stepOver(nextNode);
+        } else {
+            currentNode.next = nextNode.next;
+            nextNode.next = null;
+            stepOver(currentNode);
+        }
+    }
+
+    public LinkedList removeDuplicatesFromLinkedList(LinkedList linkedList) {
+        stepOver(linkedList);
+        return linkedList;
+    }
+
+    public static class LinkedList {
+        public int value;
+        public LinkedList next;
+
+        public LinkedList(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+}
