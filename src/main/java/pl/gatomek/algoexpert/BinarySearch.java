63fbce825418fe7bc06bbb470c70e87052997e5f
Gatomek <tomasz.galinski@gmail.com>
2026-09-19 13:50:13 +0200

    Binary Search added

---
+++ src/main/java/pl/gatomek/algoexpert/BinarySearch.java
@@ -0,0 +1,26 @@
+package pl.gatomek.algoexpert;
+
+public class BinarySearch {
+    public static int binarySearch(int[] array, int target) {
+        return binary(array, 0, array.length, target);
+    }
+
+    private static int binary(int[] array, int start, int end, int target) {
+        if (start == end) {
+            return -1;
+        }
+
+        int middle = (start + end) / 2;
+        int middleValue = array[middle];
+
+        if (middleValue == target) {
+            return middle;
+        }
+
+        if (target < middleValue) {
+            return binary(array, start, middle, target);
+        }
+
+        return binary(array, middle+1, end, target);
+    }
+}
